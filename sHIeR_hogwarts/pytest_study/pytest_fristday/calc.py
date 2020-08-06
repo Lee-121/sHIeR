08bef50bd1dc078b84f6fab0bedabe7b8c96a0cd
sHIeR <lidong27969@163.com>
2020-08-06 17:02:38 +0800

    "pytest_fristday_0805"

---
+++ sHIeR_hogwarts/pytest_study/pytest_fristday/calc.py
@@ -0,0 +1,20 @@
+# 定义Calulator类
+class Calculator:
+    # 定义add方法
+    def add(self, a, b):
+        return a + b
+
+    # 定义sub方法
+    def sub(self, a, b):
+        return a - b
+
+    # 定义mul方法
+    def mul(self, a, b):
+        return a * b
+
+    # 定义div方法
+    def div(self, a, b):
+        return a / b
+
+
+
---
+++ sHIeR_hogwarts/pytest_study/testing/test_calc.py
@@ -0,0 +1,30 @@
+from calc import Calculator
+
+
+class TestCalc:
+
+
+    def test_add1(self):
+        # 实例化Calculator
+        calc = Calculator()
+        #调用add方法
+        r = calc.add(5,6)
+        #断言
+        assert 11 == r
+
+    def test_add2(self):
+        # 实例化Calculator
+        calc = Calculator()
+        #调用add方法
+        r = calc.add(600,600)
+        #断言
+        assert 1200 == r
+
+    def test_add3(self):
+        # 实例化Calculator
+        calc = Calculator()
+        #调用add方法
+        r = calc.add(-1,-3)
+        #断言
+        assert -4 == rr
+
